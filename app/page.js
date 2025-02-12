c4d8dd35eca68f8018aa6208d3b7b5237201135b
abhishekmaliyal <87431983+abhishekmaliyal@users.noreply.github.com>
2025-02-12 20:46:18 +0530

    greetings added

--- app/page.js
+++ app/page.js
@@ -1,9 +1,22 @@
-import Image from "next/image";
+import Greetings from "@/components/Greetings";
+import TechStack from "@/components/TechStack";
 
 export default function Home() {
-    return <>
-        <div>
-            this is test
-        </div>
-    </>;
+    return (
+        <>
+            <div className="w-full h-auto text-3xl text-black py-20 px-10 justify-center items-center">
+                <div className="hello w-[800px]">
+                    <Greetings />
+                </div>
+                <div className="intro w-[800px]">
+                    <h3 className="font-bold text-3xl bg-clip-text text-transparent  bg-[hsl(0,0%,0%)]">
+                        I Am Abhishek Maliyal, A Front-End Web-Developer
+                    </h3>
+                </div>
+            </div>
+            <div className="w-full h-auto text-3xl text-black py-20 px-10 justify-center items-center">
+                <TechStack />
+            </div>
+        </>
+    );
 }
---
+++ components/TechStack.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function TechStack() {
+  return (
+    <div>
+      This is tech stack
+      <div>
+            <div>ReactJS</div>
+            <div>NextJS</div>
+            <div>HTML</div>
+            <div>CSS</div>
+          </div>
+    </div>
+  )
+}
